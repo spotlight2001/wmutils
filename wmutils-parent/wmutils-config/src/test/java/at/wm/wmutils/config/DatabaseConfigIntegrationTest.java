617757c50b2715eec8d7cfd3e1d50b97563fbe7c
walter <waltermauritz@gmx.at>
2013-10-03 09:41:10 +0200

    test added for changing value

--- wmutils-parent/wmutils-config/src/test/java/at/wm/wmutils/config/DatabaseConfigIntegrationTest.java
+++ wmutils-parent/wmutils-config/src/test/java/at/wm/wmutils/config/DatabaseConfigIntegrationTest.java
@@ -11,7 +11,22 @@ public class DatabaseConfigIntegrationTest extends AbstractTest {
 	private MessageSource messages;
 
 	@Test
-	public void test() {
-		Assert.assertEquals("http://xyz.com", messages.getMessage("app.url", null, null));
+	public void test() throws Exception {
+		// check current value by obj
+		Assert.assertEquals("http://xyz.com",
+				messages.getMessage("app.url", null, null));
+
+		// change value in db
+		Assert.assertEquals(
+				1,
+				jdbcTemplate
+						.update("update app_config set cfg_value = 'http://abc.com' where cfg_key = 'app.url'"));
+
+		// sleep longer than caching time
+		Thread.sleep(2000);
+
+		// recheck values
+		Assert.assertEquals("http://abc.com",
+				messages.getMessage("app.url", null, null));
 	}
 }
